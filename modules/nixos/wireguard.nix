9d863634bd68f5a372472a6a5a20b8dfb9d1e02a
Pavlo B <git@bpavlo.com>
2026-02-02 21:39:50 -0500

    add wireguard

---
+++ modules/nixos/wireguard.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Install WireGuard tools for manual management with wg-quick
+  environment.systemPackages = with pkgs; [
+    wireguard-tools
+  ];
+
+  # Required for WireGuard to work properly
+  networking.firewall.checkReversePath = "loose";
+}
