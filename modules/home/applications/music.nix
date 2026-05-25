126a201be1286a95f6bc3f57237b46365fd12dd7
Pavlo B <git@bpavlo.com>
2026-05-24 21:36:16 -0400

    feat(music): drop scribble

--- modules/home/applications/music.nix
+++ modules/home/applications/music.nix
@@ -15,7 +15,7 @@
       directory = "${config.home.homeDirectory}/Music";
       library = "${config.xdg.dataHome}/beets/library.db";
 
-      plugins = "musicbrainz fetchart embedart replaygain scrub info missing smartplaylist bandcamp duplicates play mbsync inline";
+      plugins = "musicbrainz fetchart embedart replaygain scrub info missing smartplaylist bandcamp duplicates play mbsync inline convert";
 
       import = {
         copy = true;
@@ -33,6 +33,21 @@
 
       embedart.auto = true;
       replaygain.backend = "ffmpeg";
+
+      convert = {
+        auto = false;
+        dest = "${config.home.homeDirectory}/mp3";
+        threads = 4;
+        embed = true;
+        copy_album_art = true;
+        album_art_maxwidth = 1000;
+        format = "mp3";
+        formats.mp3 = {
+          command = "${pkgs.jellyfin-ffmpeg}/bin/ffmpeg -i $source -y -map 0:a -map_metadata 0 -c:a libmp3lame -b:a 320k -compression_level 0 -ar 44100 $dest";
+          extension = "mp3";
+        };
+        paths.default = "$albumartist/$album%aunique{} ($year)/$track $title";
+      };
     };
   };
 
