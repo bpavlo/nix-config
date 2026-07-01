dd5ca9b40903b033cacd2e4f0a39e4449db51c7d
Pavlo B <git@bpavlo.com>
2026-06-30 21:08:12 -0400

    bluetooth: sbc

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -40,22 +40,23 @@ in
           "bluez5.enable-sbc-xq" = true;
           "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" =
-            if bluetoothCfg.playbackOnly && !bluetoothCfg.allowHeadsetProfile then
-              [
-                "a2dp_sink"
-                "a2dp_source"
-              ]
-            else
-              [
-                "a2dp_sink"
-                "a2dp_source"
-                "hsp_hs"
-                "hsp_ag"
-                "hfp_hf"
-                "hfp_ag"
-              ];
+          "bluez5.roles" = [
+            "a2dp_sink"
+            "a2dp_source"
+          ];
         };
+
+        "monitor.bluez.rules" = [
+          {
+            matches = [ { "device.name" = "~bluez_card.*"; } ];
+            actions.update-props = {
+              "bluez5.codecs" = [
+                "sbc_xq"
+                "sbc"
+              ];
+            };
+          }
+        ];
       };
     };
 
